9bcf734c336e3a898674d4d74fcebbb4233a1bf2
Chirag gour <chiragvgour@gmail.com>
2024-11-11 19:02:34 +0530

    First Commit

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Player
+    {
+        public int id;
+        public string name;
+        public int health;
+        public void details(string name, int health)
+        {
+            this.name = name;
+            this.health = health;
+            Console.WriteLine($"your player name {name} and health {health} is set!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write(args[i]);
+            }
+
+            Console.WriteLine("Enter Your Age?");
+
+            string age = Console.ReadLine();
+
+            //int legalage = Convert.ToInt32(age); //throws exception/error if input is not valid number 
+
+            bool checkAge = int.TryParse(age, out int legalage); // more safe method to conversion, no exception/error thrown
+            if (checkAge)
+            {
+
+                if (legalage >= 18)
+                {
+                    Console.WriteLine("Congrats You Will have to start working");
+                }
+                else
+                {
+                    Console.WriteLine("Shut up and go study kid!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number, not a string!");
+            }
+
+            Player user = new Player();
+            Console.WriteLine("Enter Player Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Player Health");
+            string healthInput = Console.ReadLine();
+            int health;
+            if (int.TryParse(healthInput, out health))
+            {
+                user.details(name, health);
+            }
+            else
+            {
+                Console.WriteLine("Invalid health input. Please enter a valid number.");
+            }
+
+        }
+    }
+}
